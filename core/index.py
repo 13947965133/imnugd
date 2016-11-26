a3660decc9dc1b1f15cd39b0eda529a23e6d2504
guohan <13947965133@163.com>
2016-11-26 21:42:24 +0800

    ajax返回的json解析问题
    ajax返回的数据可以显示了
    json解析问题的问题尚未解决

--- core/index.py
+++ core/index.py
@@ -12,15 +12,17 @@ class MainHandler(tornado.web.RequestHandler):
          a = self.get_argument('a')
          data = parser.index(a)
          print core.index(data)
+         print a
          lihui = "11111"
          self.render('list.json',lihui=lihui)
 
 class UsersMandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("hello UsersMandler")
-        print "papapa"
+        self.write('''[{"content":"hencucuo","id":1,"nickname":"nani"},{"content":"youxi","id":2,"nickname":"xiaoqiang"}]''')
+        print "write success"
     def post(self):
         data = self.get_argument('a')
+        data = parser.index(a)
         print core.index(data)
 
 class DingDanMandler(tornado.web.RequestHandler):
