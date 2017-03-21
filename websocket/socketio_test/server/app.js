7720f1256fa4725fb222a437bfad5b4022a615ae
guohan <13947965133@163.com>
2017-03-21 11:19:26 +0800

    websocket.io调通

---
+++ websocket/socketio_test/server/app.js
@@ -0,0 +1,26 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+app.listen(8888);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello2: 'worldwww' });
+  // socket.on('my other event', function (data) {
+  //   console.log(data);
+  // });
+});
+      
