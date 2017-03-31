bfb44e34e23774e6ee7936742943d1a84203e9cd
guohan <13947965133@163.com>
2017-03-31 14:43:46 +0800

    修改socket.io
    增加了dingdan的sql语句

---
+++ db/postgres/dingdan.sql
@@ -0,0 +1,25 @@
+创建的订单表
+
+CREATE TABLE  ddb(
+   sid varchar(16),
+   zhuohao int,
+   list json,
+   beizhu text,
+   sfzf   boolean,
+   fapiao boolean,
+   sj     timestamp,
+
+);
+
+插入的记录
+
+INSERT INTO ddb (list,sid,zhuohao,beizhu,sfzf,sj,fapiao,sum)
+VALUES ('[
+  {"name":["num","total","size","attr1"]}
+  ,{"商品名称":["个数","总价","规格","属性"]}
+
+]',
+'18947129292',003,'',TRUE,'2017-1-31 13:20:07',TRUE,80);
+
+查询商家
+select * from ddb where sid='18947129292';
